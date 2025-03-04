aa4d86de95534f90b8f17ffbed9c4c3e3ac5d007
Bilgin Bilir <bilgin.bilir@whu.edu>
2025-03-04 14:47:09 +0100

    Ch.2 / show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,18 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    find_task
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
